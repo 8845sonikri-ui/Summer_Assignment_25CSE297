68eab4f413b4656ee2932186ee91d1e44b43a4ea
suhani saraswat <saraswatsuhani3@gmail.com>
2026-07-13 20:05:18 +0530

    Day 14

---
+++ Day 14/Q53.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, key, found = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter number to search: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            found = 1;
+            break;
+        }
+    }
+
+    if(found)
+        printf("Element found at index %d", i);
+    else
+        printf("Element not found");
+
+    return 0;
+}
