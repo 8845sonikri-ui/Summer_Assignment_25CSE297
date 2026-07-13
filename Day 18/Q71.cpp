59e7dec3811f3028348a7e3c366ceda025a432c8
suhani saraswat <saraswatsuhani3@gmail.com>
2026-07-13 20:21:45 +0530

    Day 18

---
+++ Day 18/Q71.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, key;
+    int low = 0, mid, found = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            found = 1;
+            break;
+        }
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    if(found)
+        printf("Element found at index %d", mid);
+    else
+        printf("Element not found");
+
+    return 0;
+}
